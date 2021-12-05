81e76652896c075cc96e7fe707dd6268747ad29d
ygkn <2000ygkn0713@gmail.com>
2021-12-05 17:16:38 +0900

    backend のマルチユーザー対応

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -3,12 +3,14 @@ import { createServer } from "http";
 import { createServer as createServerTLS } from "https";
 import { WebSocket, WebSocketServer } from "ws";
 import express from "express";
+import { nanoid } from "nanoid";
 
 const PORT = parseInt(process.env.PORT ?? "8080");
 const HOST = process.env.HOST ?? "0.0.0.0";
 
 const main = async () => {
   const listeners = new Set<WebSocket>();
+  const clientIds = new Map<WebSocket, string>();
 
   const app = express();
 
@@ -24,6 +26,8 @@ const main = async () => {
   wss.on("error", console.log);
 
   wss.on("connection", function (client: WebSocket) {
+    clientIds.set(client, nanoid());
+
     client.on("message", function (message: string) {
       const obj = JSON.parse(message);
 
@@ -33,13 +37,27 @@ const main = async () => {
 
       if (typeof obj.type === "string" && obj.type === "broadcast") {
         for (const listener of listeners) {
-          listener.send(message);
+          listener.send(
+            JSON.stringify({
+              ...obj,
+              uid: clientIds.get(client),
+              type: "orientation",
+            })
+          );
         }
       }
     });
 
     client.on("close", () => {
       listeners.delete(client);
+      for (const listener of listeners) {
+        listener.send(
+          JSON.stringify({
+            uid: clientIds.get(client),
+            type: "disconnect",
+          })
+        );
+      }
     });
   });
 
