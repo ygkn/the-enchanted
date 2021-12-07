ee34f21554b4488c1d080a0315b4739c1386db96
ygkn <2000ygkn0713@gmail.com>
2021-12-07 19:24:24 +0900

    fix config

--- packages/screen/vite.config.ts
+++ packages/screen/vite.config.ts
@@ -1,13 +1,11 @@
 import { UserConfigExport } from "vite";
 import path from "path";
 import devcert from "devcert";
-import reactJsx from "vite-react-jsx";
 
 export default async (): Promise<UserConfigExport> => {
   const { key, cert } = await devcert.certificateFor("localhost");
 
   return {
-    plugins: [reactJsx()],
     resolve: {
       alias: {
         "@/": path.join(__dirname, "./src/"),
